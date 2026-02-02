f9864acd70a7fbbc2717c8328059743ecea01710
ziad49 <ziad.freij@isen-ouest.yncrea.fr>
2026-02-02 13:29:49 +0100

    update src/logic.js

--- src/logic.js
+++ src/logic.js
@@ -1,9 +1,7 @@
-const getProfileData = () => {
-    return {
-        name: "Ziad FREIJ",
-        email: "ziadfr2002@gmail.com",
-        status: "Stage trouvé - En poste"
-    };
+export const getProfileData = () => {
+  return {
+    name: "Ziad FREIJ",
+    email: "ziadfr2002@gmail.com",
+    status: "Stage trouvé - En poste",
+  };
 };
-
-module.exports = { getProfileData };
